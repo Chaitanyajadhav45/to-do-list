ca240cc439f25965505a3a8ecee2b533cb820c3b
Chaitanyajadhav45 <chaitanyajadhav715@gmail.com>
2024-07-12 21:25:50 +0530

    Add

--- script.js
+++ script.js
@@ -7,15 +7,7 @@ if(listFromLocalStorage)
     renderToDoList();
 }
 
-function add(){
-    const inputElement = document.getElementById('input-todo');
-    todoList.push(inputElement.value);
-    
-    localStorage.setItem("todoList", JSON.stringify(todoList));
-    inputElement.value = "";
 
-    renderToDoList();
-}
 
 function renderToDoList(){
     const listContainer = document.getElementById('list-container');
